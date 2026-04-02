806cfd35e51ce4a68ca7923cb97fec9444d9a768
Rohit Yadav <rohityadav.0620@gmail.com>
2026-04-03 00:57:50 +0530

    Delete backend/vad_2 directory

--- backend/vad_2/intent_1/example_run_live.py
+++
@@ -1,5 +0,0 @@
-from live_listener import LiveRunner
-
-if __name__ == "__main__":
-    runner = LiveRunner()
-    runner.start()
--- backend/vad_2/pipline/intent_stage.py
+++
@@ -1,9 +0,0 @@
-# vad/pipline/intent_stage.py
-from vad.intent_1.hybrid_classifier import HybridIntentClassifier
-
-class IntentStage:
-    def __init__(self):
-        self.classifier = HybridIntentClassifier()
-
-    def classify(self, text):
-        return self.classifier.predict(text)
